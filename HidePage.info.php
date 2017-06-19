3566815fb7d8faab4827b4d2129087132668bc6b
Daniel Ehniss <daniel@depone.de>
2017-06-19 13:16:06 +0200

    Add URL of Repo to info-file

--- HidePage.info.php
+++ HidePage.info.php
@@ -4,7 +4,7 @@ $info = array(
 	'title' => 'Hide Page',
 	'version' => 1,
 	'summary' => 'Set status hidden on pages that are used with a specific template.',
-	'href' => 'https://depone.net',
+	'href' => 'https://github.com/deponeWD/HidePage',
 	'singular' => true,
 	'autoload' => true,
 );
